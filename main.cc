5c2b4122c301336f2801a1edc73d840b4cc5cb12
Ankur Mittal <anmittal@google.com>
2016-07-26 18:18:23 -0700

    Add build system and our test runner for gtests
    This will create a binary names libc-tests which is a empty gtest
    
    Change-Id: I6b2d030f48573d8cc84e6c208301d240b96e30d8

--- main.cc
+++ main.cc
@@ -1,8 +1,10 @@
-// Copyright 2016 The Native Client Authors. All rights reserved.
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "gtest/gtest.h"
+#include <stdio.h>
+
 
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
