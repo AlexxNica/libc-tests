e5f20b55aedae083d791c880007bbd9dea65c90e
Ankur Mittal <anmittal@google.com>
2016-07-29 17:17:29 -0700

    [libc-tests] Port ABI tests
    Change-Id: I214ab0e5419b0dd95d4f96c19e62c93bbfa419b8

--- third_party/nacl-ported-tests/abi/abi.cc
+++ third_party/nacl-ported-tests/abi/abi.cc
@@ -21,17 +21,30 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 
-#include "native_client/src/public/imc_types.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+class AbiTests : public ::testing::Test {
+ protected:
+
+  AbiTests() {
+    // You can do set-up work for each test here.
+  }
+
+  ~AbiTests() override {
+  }
 
 
-int Assert(int actual, int expected, const char* mesg) {
-  if (actual == expected) return 0;
+  void SetUp() override {
+  }
 
-  printf("ERROR: %s\n", mesg);
-  printf("expected: %d, got: %d\n", expected, actual);
+  void TearDown() override {
+  }
+};
 
-  return 1;
-}
+} //namespace
+
 
 /* we expect this to be padded to 16 bytes of total size */
 typedef struct {
@@ -39,8 +52,7 @@ typedef struct {
   float f;
 } S1;
 
-
-#define CHECK_SIZEOF(t, v) nerror += Assert(sizeof(t), v, "bad sizeof " #t)
+#define CHECK_SIZEOF(t, v) EXPECT_EQ(sizeof(t), (size_t)v) << "bad sizeof " #t
 
 /* Helper Types so we can describe a type in one token */
 typedef void (*FunctionPointer)(void);
@@ -48,21 +60,19 @@ typedef void *Pointer;
 typedef long long long_long;
 typedef long double long_double;
 
-int CheckSizes(void) {
-  int nerror = 0;
-
+TEST_F(AbiTests, CheckSizes) {
   CHECK_SIZEOF(char, 1);
   CHECK_SIZEOF(short, 2);
   CHECK_SIZEOF(int, 4);
-  CHECK_SIZEOF(long, 4);
+  CHECK_SIZEOF(long, 8);
   CHECK_SIZEOF(long long, 8);
 
-  CHECK_SIZEOF(Pointer, 4);
-  CHECK_SIZEOF(FunctionPointer, 4);
+  CHECK_SIZEOF(Pointer, 8);
+  CHECK_SIZEOF(FunctionPointer, 8);
 
   CHECK_SIZEOF(float, 4);
   CHECK_SIZEOF(double, 8);
-  CHECK_SIZEOF(long_double, 8);
+  CHECK_SIZEOF(long_double, 16);
 
   CHECK_SIZEOF(S1, 16);
   CHECK_SIZEOF(S1[2], 16*2);
@@ -71,79 +81,57 @@ int CheckSizes(void) {
 
   CHECK_SIZEOF(ino_t, 8);
   CHECK_SIZEOF(mode_t, 4);
-  CHECK_SIZEOF(nlink_t, 4);
+  CHECK_SIZEOF(nlink_t, 8);
   CHECK_SIZEOF(uid_t, 4);
   CHECK_SIZEOF(gid_t, 4);
   CHECK_SIZEOF(off_t, 8);
-  CHECK_SIZEOF(blksize_t, 4);
-  CHECK_SIZEOF(blkcnt_t, 4);
+  CHECK_SIZEOF(blksize_t, 8);
+  CHECK_SIZEOF(blkcnt_t, 8);
 
   CHECK_SIZEOF(off_t, 8);
-  CHECK_SIZEOF(size_t, 4);
-  CHECK_SIZEOF(fpos_t, 4);
+  CHECK_SIZEOF(size_t, 8);
+  CHECK_SIZEOF(fpos_t, 16);
 
   CHECK_SIZEOF(time_t, 8);
   CHECK_SIZEOF(struct timezone, 8);
-  CHECK_SIZEOF(suseconds_t, 4);
-  CHECK_SIZEOF(clock_t, 4);
+  CHECK_SIZEOF(suseconds_t, 8);
+  CHECK_SIZEOF(clock_t, 8);
   CHECK_SIZEOF(clockid_t, 4);
   CHECK_SIZEOF(struct timeval, 16);
   CHECK_SIZEOF(struct timespec, 16);
 
-  CHECK_SIZEOF(struct stat, 104);
+  CHECK_SIZEOF(struct stat, 144);
   CHECK_SIZEOF(struct dirent, 280);
 
-  CHECK_SIZEOF(struct NaClAbiNaClImcMsgHdr, 20);
-
-#ifdef PNACL_ABI_TEST
-  /* BUG: http://code.google.com/p/nativeclient/issues/detail?id=2203 */
-  /* the idea behind picking such an insanely high value
-     is to be prepapared for crazy architectures */
-  /* current values are:
-     nacl-gcc (newlib): 36
-     nacl64-gcc (newlib):  48
-     Note: Itanium uses 560 bytes
-  */
-  CHECK_SIZEOF(jmp_buf, 1024);
-#endif
-  return nerror;
+  CHECK_SIZEOF(jmp_buf, 200);
 }
 
 
 #define CHECK_ALIGNMENT(T, a) do { \
   typedef struct { char c; T  x; } AlignStruct; \
-  nerror += Assert(offsetof(AlignStruct, x), a, "bad offsetof " #T); \
+  EXPECT_EQ(offsetof(AlignStruct, x), (size_t)a) << "bad offsetof " #T; \
   } while(0)
 
-int CheckAlignment(void) {
-  int nerror = 0;
+TEST_F(AbiTests, CheckAlignment) {
   CHECK_ALIGNMENT(char, 1);
   CHECK_ALIGNMENT(short, 2);
   CHECK_ALIGNMENT(int, 4);
-  CHECK_ALIGNMENT(long, 4);
+  CHECK_ALIGNMENT(long, 8);
   CHECK_ALIGNMENT(float, 4);
   CHECK_ALIGNMENT(double, 8);
-  CHECK_ALIGNMENT(long_double, 8);
-  CHECK_ALIGNMENT(Pointer, 4);
-  CHECK_ALIGNMENT(FunctionPointer, 4);
+  CHECK_ALIGNMENT(long_double, 16);
+  CHECK_ALIGNMENT(Pointer, 8);
+  CHECK_ALIGNMENT(FunctionPointer, 8);
   CHECK_ALIGNMENT(long_long, 8);
 
   CHECK_ALIGNMENT(struct stat, 8);
   CHECK_ALIGNMENT(struct dirent, 8);
   CHECK_ALIGNMENT(struct timeval, 8);
   CHECK_ALIGNMENT(struct timespec, 8);
-
-#ifdef PNACL_ABI_TEST
-  /* BUG: http://code.google.com/p/nativeclient/issues/detail?id=2203 */
-  /* TODO: we may want to switch this 16 (used by Itanium) */
-  /* NOTE: alignment for nacl-gcc differs between x86-32/64 */
-  CHECK_ALIGNMENT(jmp_buf, 8);
-#endif
-  return nerror;
 }
 
 
-int AssertMemcmp(char* name, unsigned char* p1, unsigned char* p2, int n) {
+int AssertMemcmp(const char* name, unsigned char* p1, unsigned char* p2, int n) {
   int i;
   if (0 ==  memcmp(p1, p2, n)) {
     return 0;
@@ -167,17 +155,16 @@ int AssertMemcmp(char* name, unsigned char* p1, unsigned char* p2, int n) {
 
 #define CHECK_BITFIELD(s1, s2, s3, v1, v2, v3, n, s) do { \
     typedef struct { unsigned f1: s1; unsigned f2:  s2; unsigned f3: s3; } BF; \
-    char* name = #s1 ":" #s2 ":" #s3 "::" #v1 ":" #v2 ":" #v3; \
+    const char* name = #s1 ":" #s2 ":" #s3 "::" #v1 ":" #v2 ":" #v3; \
     BF bf;                          \
     memset(&bf, 0, sizeof(BF)); \
     bf.f1 = v1; bf.f2 = v2; bf.f3 = v3; \
-    nerror += Assert(sizeof(bf), n, "unexpected bitfield size"); \
-    nerror += AssertMemcmp(name, (unsigned char *)&bf, (unsigned char *)s, n); \
+    EXPECT_EQ(sizeof(bf), (size_t)n) << "unexpected bitfield size"; \
+    EXPECT_EQ(AssertMemcmp(name, (unsigned char *)&bf, (unsigned char *)s, n), 0); \
   } while(0)
 
 
-int CheckBitfields(void) {
-  int nerror = 0;
+TEST_F(AbiTests, CheckBitfields) {
   CHECK_BITFIELD(1, 1, 1,  0, 0, 1,  4,
                  "\x04\0\0\0");
   CHECK_BITFIELD(2, 2, 2,  1, 2, 3,  4,
@@ -191,13 +178,4 @@ int CheckBitfields(void) {
   CHECK_BITFIELD(31, 31, 31,  1, 2, 3,  12,
                  "\x01\x00\x00\x00\x02\x00\x00\x00\x03\x00\x00\x00");
 
-  return nerror;
-}
-
-int main(int argc, char* argv[]) {
-  int nerror = 0;
-  nerror += CheckSizes();
-  nerror += CheckAlignment();
-  nerror += CheckBitfields();
-  return nerror;
 }
