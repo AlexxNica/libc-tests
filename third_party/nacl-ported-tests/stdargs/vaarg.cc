8f8237eb679726c84b78e4cd4864281fbcd4bdca
Ankur Mittal <anmittal@google.com>
2016-08-05 15:36:18 -0700

    [libc-tests] Port vaarg test
    Change-Id: I232559ec11f352136def991999f2b33285c5d87c

--- third_party/nacl-ported-tests/stdargs/vaarg.cc
+++ third_party/nacl-ported-tests/stdargs/vaarg.cc
@@ -8,106 +8,101 @@
  * NaCl test for super simple program not using newlib
  */
 
-#include "barebones.h"
 #include <stdarg.h>
+#include "gtest/gtest.h"
+
+namespace {
+
+class StdArgsTests : public ::testing::Test {
+ protected:
+
+  StdArgsTests() {
+    // You can do set-up work for each test here.
+  }
+
+  ~StdArgsTests() override {
+  }
+
+
+  void SetUp() override {
+  }
+
+  void TearDown() override {
+  }
+};
+
+} //namespace
+
+
 
 /* globals are intentional to confuse optimizer */
 int errors = 55;
 int count = 0;
 char buffer[16];
 
-int cmp(long l) {
-  ++count;
-
-  if (l != count) {
-    myprint("Error is ");
-    myhextochar(l, buffer);
-    myprint(buffer);
-    myprint(" expected ");
-    myhextochar(count, buffer);
-    myprint(buffer);
-    myprint("\n");
-
-    return 1;
-  }
-
-  return 0;
-}
-
-
-int dump(const char *fmt, ...) {
-  int errors = 0;
+void dump(const char *fmt, ...) {
   int i;
 
   va_list ap;
   va_start(ap, fmt);
 
-  myprint(fmt);
-  myprint("\n");
   count = 0;
 
   for (i = 0; fmt[i]; ++i) {
     if (fmt[i] == 'L') {
       long l = va_arg(ap, long);
-      errors += cmp(l);
+      count++;
+      EXPECT_EQ(l, count);
     } else if (fmt[i] == 'Q') {
       long long ll = (long long) va_arg(ap, long long);
-      errors += cmp((long)((ll >> 32) & 0xffffffffL));
-      errors += cmp((long)(ll & 0xffffffffL));
+      count++;
+      EXPECT_EQ(((long)((ll >> 32) & 0xffffffffL)), count);
+      count++;
+      EXPECT_EQ(((long)(ll & 0xffffffffL)), count);
     } else {
-      myprint("Error bad format");
-      errors += 100;
+      // Bad format letter!
+      EXPECT_TRUE(false) << "Bad format letter: " << fmt[i];
     }
   }
   va_end(ap);
-
-  return errors;
 }
 
 
-void test(void) {
-  errors += dump("QQQL",
-                 0x0000000100000002LL,
-                 0x0000000300000004LL,
-                 0x0000000500000006LL,
-                 0x00000007L);
+TEST_F(StdArgsTests, TestVaargs) {
+  dump("QQQL",
+       0x0000000100000002LL,
+       0x0000000300000004LL,
+       0x0000000500000006LL,
+       0x00000007L);
 
-  errors += dump("QQLQ",
-                 0x0000000100000002LL,
-                 0x0000000300000004LL,
-                 0x00000005L,
-                 0x0000000600000007LL);
+  dump("QQLQ",
+       0x0000000100000002LL,
+       0x0000000300000004LL,
+       0x00000005L,
+       0x0000000600000007LL);
 
-  errors += dump("QLQQ",
-                 0x0000000100000002LL,
-                 0x00000003L,
-                 0x0000000400000005LL,
-                 0x0000000600000007LL);
+  dump("QLQQ",
+       0x0000000100000002LL,
+       0x00000003L,
+       0x0000000400000005LL,
+       0x0000000600000007LL);
 
-  errors += dump("LQQQ",
-                 0x00000001L,
-                 0x0000000200000003LL,
-                 0x0000000400000005LL,
-                 0x0000000600000007LL);
+  dump("LQQQ",
+       0x00000001L,
+       0x0000000200000003LL,
+       0x0000000400000005LL,
+       0x0000000600000007LL);
 
-  errors += dump("QQQ",
-                 0x0000000100000002LL,
-                 0x0000000300000004LL,
-                 0x0000000500000006LL);
+  dump("QQQ",
+       0x0000000100000002LL,
+       0x0000000300000004LL,
+       0x0000000500000006LL);
 
-  errors += dump("LLLLLL",
-                 0x00000001L,
-                 0x00000002L,
-                 0x00000003L,
-                 0x00000004L,
-                 0x00000005L,
-                 0x00000006L);
-}
-
-
-int main(void) {
-  test();
-  NACL_SYSCALL(exit)(errors);
-  /* UNREACHABLE */
-  return 0;
+  dump("LLLLLL",
+       0x00000001L,
+       0x00000002L,
+       0x00000003L,
+       0x00000004L,
+       0x00000005L,
+       0x00000006L);
 }
