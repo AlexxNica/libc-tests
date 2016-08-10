fcb1b468a589dda08013b148de8ee258f437dd26
Ankur Mittal <anmittal@google.com>
2016-08-10 16:31:10 -0700

    [libc-tests] Port sleep test
    Change-Id: I3f01e07dce52f471707e1cb9874929ae7aa00d50

--- third_party/nacl-ported-tests/sleep/sleep_test.cc
+++ third_party/nacl-ported-tests/sleep/sleep_test.cc
@@ -7,8 +7,36 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(void) {
+#include "gtest/gtest.h"
+
+namespace {
+
+class SleepTests : public ::testing::Test {
+ protected:
+
+  SleepTests() {
+    // You can do set-up work for each test here.
+  }
+
+  ~SleepTests() override {
+  }
+
+
+  void SetUp() override {
+  }
+
+  void TearDown() override {
+  }
+};
+
+} //namespace
+
+TEST_F(SleepTests, TestSleep) {
+  clock_t start, end;
+  start = time(NULL);
   unsigned int unslept = sleep(1);
-  printf("unslept %d\n", unslept);
-  return unslept == 0 ? 0 : 1;
+  end = time(NULL);
+  time_t elapsed_time = (end - start);
+  EXPECT_EQ(elapsed_time, 1); // Should have slept for 1 sec
+  EXPECT_EQ(unslept, 0u);
 }
