fb74faf8afcdcc108d2db81a81e64e44c009764b
Ankur Mittal <anmittal@google.com>
2016-08-18 14:51:50 -0700

    [libc-tests] Port strtoull tests
    Change-Id: I90a312a7caccf923f00a8218035ac61d41b07f2c

--- third_party/nacl-ported-tests/libc/strtoull.cc
+++ third_party/nacl-ported-tests/libc/strtoull.cc
@@ -10,24 +10,56 @@
 #include <stdlib.h>
 #include <string.h>
 
-int test(const char *str, uint64_t want, int base) {
+#include "gtest/gtest.h"
+
+namespace {
+
+struct test_param {
+  const char *str;
+  uint64_t want;
+  int base;
+};
+
+class StrToullTests : public ::testing::TestWithParam<struct test_param> {
+ protected:
+
+  StrToullTests() {
+    // You can do set-up work for each test here.
+  }
+
+  ~StrToullTests() override {
+  }
+
+
+  void SetUp() override {
+  }
+
+  void TearDown() override {
+  }
+};
+
+} //namespace
+
+
+TEST_P(StrToullTests, TestStrToull) {
+  const struct test_param *params = &GetParam();
+  const char *str = params->str;
+  uint64_t want = params->want;
+  int base = params->base;
   uint64_t val;
   errno = 0; /* errno is undefined on success unless initialized */
   val = strtoull(str, 0, base);
-  if (errno != 0) {
-    fprintf(stderr, "strtoull(\"%s\", 0, %d): %s\n",
-        str, base, strerror(errno));
-    return 1;
-  }
-  if (val == want)
-    return 0;
-  fprintf(stderr, "%llu != %llu\n", val, want);
-  return 1;
+  EXPECT_EQ(errno, 0) << "strtoull(" << str << ", 0, "
+                      << base << "):", strerror(errno);
+  ASSERT_EQ(val, want);
 }
 
-int main(void) {
-  int errors = 0;
-  errors += test("5", 5, 10);
-  errors += test("FFFFFFFFFFFFFFFF", 0xFFFFFFFFFFFFFFFFLL, 16);
-  return errors;
-}
+struct test_param params[] = {
+    { "5", 5, 10 },
+    { "FFFFFFFFFFFFFFFF", 0xFFFFFFFFFFFFFFFFLL, 16 }
+};
+
+INSTANTIATE_TEST_CASE_P(StrToull,
+                        StrToullTests,
+                        ::testing::ValuesIn(params));
+
