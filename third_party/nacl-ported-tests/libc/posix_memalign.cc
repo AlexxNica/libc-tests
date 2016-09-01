bf661c4dff902a61c32faa0e8e96e9edc89b7a79
Ankur Mittal <anmittal@google.com>
2016-08-31 18:01:33 -0700

    Port Posix memalign test
    Change-Id: I0575e6ce22d33c6d726674a85b922aed9636b4f1

--- third_party/nacl-ported-tests/libc/posix_memalign.cc
+++ third_party/nacl-ported-tests/libc/posix_memalign.cc
@@ -7,12 +7,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) {
+#include "gtest/gtest.h"
+
+namespace {
+
+class PosixMemAlignTests : public ::testing::Test {
+ protected:
+
+  PosixMemAlignTests() {
+    // You can do set-up work for each test here.
+  }
+
+  ~PosixMemAlignTests() override {
+  }
+
+
+  void SetUp() override {
+  }
+
+  void TearDown() override {
+  }
+};
+
+} //namespace
+
+TEST_F(PosixMemAlignTests, TestPosixMemAlign) {
   char *outp;
   /* Try a few alignments, some of them might be the standard malloc align.
    * They should all be powers of 2 and be a multiple of sizeof(void*).
    */
-  size_t align_to_test[] = {4, 8, 16, 32, 64, 128, 256, 512, 1024, 0};
+  size_t align_to_test[] = {8, 16, 32, 64, 128, 256, 512, 1024, 0};
   size_t sizes_to_test[] = {1, 2, 4, 8, 10, 16, 32, 64, 128, 256, 512, 1024, 0};
   int i = 0;
   int j = 0;
@@ -23,19 +47,11 @@ int main(int argc, char* argv[]) {
       int err = posix_memalign((void **)&outp,
                                align_to_test[i],
                                sizes_to_test[j] * sizeof(char));
-      if (err) {
-        fprintf(stderr,
-                "posix_memalign failed with err %d (EINVAL=%d,ENOMEM=%d)\n",
-                err, EINVAL, ENOMEM);
-        fprintf(stderr, "Input params were align=%d, size=%d\n",
-                align_to_test[i], sizes_to_test[j]);
-        return 1;
-      }
-      if ((size_t)outp % align_to_test[i] != 0) {
-        fprintf(stderr, "posix_memalign failed to align to %zu: ptr=%p\n",
-                align_to_test[i], (void *) outp);
-        return 1;
-      }
+      ASSERT_EQ(err, 0) << "Input params were align=" << align_to_test[i]
+                        << " size=" << sizes_to_test[j];
+      ASSERT_EQ((size_t)outp % align_to_test[i], 0ul)
+          << "posix_memalign failed to align to " << align_to_test[i]
+          << ": ptr=" << (void *) outp;
       free(outp);
       j++;
     }
@@ -43,17 +59,11 @@ int main(int argc, char* argv[]) {
   }
 
   /* Check that a non-power of 2 alignment fails. */
-  if (posix_memalign((void**)&outp, 7, 20 * sizeof(char)) != EINVAL) {
-    fprintf(stderr, "posix_memaligned failed to return EINVAL for non-pow2!\n");
-    return 1;
-  }
+  ASSERT_EQ(posix_memalign((void**)&outp, 7, 20 * sizeof(char)), EINVAL)
+      << "posix_memaligned failed to return EINVAL for non-pow2!";
 
   /* Check that smaller than sizeof(void*) alignment fails. */
-  if (posix_memalign((void**)&outp, sizeof(void*) - 1,
-                     20 * sizeof(char)) != EINVAL) {
-    fprintf(stderr, "posix_memaligned failed to return EINVAL for non-pow2!\n");
-    return 1;
-  }
-
-  return 0;
+  ASSERT_EQ(posix_memalign((void**)&outp, sizeof(void*) - 1,
+                     20 * sizeof(char)), EINVAL)
+      << "posix_memaligned failed to return EINVAL for non-pow2!";
 }
