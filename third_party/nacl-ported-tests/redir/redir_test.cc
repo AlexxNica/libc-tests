70a88a46a6dfaeda8f3e13ce9ef0e15fe48d288b
Ankur Mittal <anmittal@google.com>
2016-09-16 16:58:54 -0700

    Port redir test
    Change-Id: Ib4fd76c79721b05383d59b8b3bdbfc1408ed745e

--- third_party/nacl-ported-tests/redir/redir_test.cc
+++ third_party/nacl-ported-tests/redir/redir_test.cc
@@ -4,10 +4,44 @@
  * found in the LICENSE file.
  */
 
+#include "gtest/gtest.h"
 #include <stdio.h>
+#include <string>
 
-int main(void) {
-  printf("REALOUTPUT: Hello standard output\n");
-  fprintf(stderr, "REALOUTPUT: Hello standard error!\n");
-  return 0;
+namespace {
+
+class RedirTests : public ::testing::Test {
+ protected:
+
+  RedirTests() {
+    // You can do set-up work for each test here.
+  }
+
+  ~RedirTests() override {
+  }
+
+
+  void SetUp() override {
+  }
+
+  void TearDown() override {
+  }
+};
+
+} //namespace
+
+
+TEST_F(RedirTests, TestRedir) {
+  const char* stdout_str = "REALOUTPUT: Hello standard output\n";
+  const char*  stderr_str = "REALOUTPUT: Hello standard error!\n";
+
+  testing::internal::CaptureStdout();
+  printf("%s", stdout_str);
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_STREQ(output.c_str(), stdout_str);
+
+  testing::internal::CaptureStderr();
+  fprintf(stderr, "%s", stderr_str);
+  output = testing::internal::GetCapturedStderr();
+  EXPECT_STREQ(output.c_str(), stderr_str);
 }
