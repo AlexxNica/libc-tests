094c791442c03a85b361edc32f262534419ae6c5
Ankur Mittal <anmittal@google.com>
2016-09-28 17:38:25 -0700

    Port float tests
    Change-Id: I29fc7b65962dd9fbf40535cce323642576c28332

--- third_party/nacl-ported-tests/math/float.cc
+++ third_party/nacl-ported-tests/math/float.cc
@@ -22,18 +22,129 @@
 #define PRINT_INT(mesg, val)\
   printf(mesg ": %d\n", (int) (val))
 
+#include "gtest/gtest.h"
 
-/* TODO(robertm): maybe use macro */
-int main_double(int argc, char* argv[]) {
+namespace {
+
+class FloatTests : public ::testing::Test {
+ protected:
+  FloatTests() {
+    // You can do set-up work for each test here.
+  }
+
+  ~FloatTests() override {}
+
+  void SetUp() override {}
+
+  void TearDown() override {}
+};
+
+}  // namespace
+
+static std::string float_output[] = {
+    R"(val str: 1.0
+val int: 1
+val flt: 1.000000000
+last: 0.000
++: 1.000
+-: -1.000
+*: 0.000
+/: 0.000
+)",
+    R"(val str: 3.0
+val int: 3
+val flt: 3.000000000
+last: 1.000
++: 4.000
+-: -2.000
+*: 3.000
+/: 0.333
+)",
+    R"(val str: 0.5
+val int: 0
+val flt: 0.500000000
+last: 3.000
++: 3.500
+-: 2.500
+*: 1.500
+/: 6.000
+)",
+    R"(val str: 100.0
+val int: 100
+val flt: 100.000000000
+last: 0.500
++: 100.500
+-: -99.500
+*: 50.000
+/: 0.005
+)",
+    R"(val str: 0.3
+val int: 0
+val flt: 0.300000012
+last: 100.000
++: 100.300
+-: 99.700
+*: 30.000
+/: 333.333
+)"};
+
+static std::string double_output[5] = {
+    R"(val str: 1.0
+val int: 1
+val flt: 1.000000000
+last: 0.000000000
++: 1.000000000
+-: -1.000000000
+*: 0.000000000
+/: 0.000000000
+)",
+    R"(val str: 3.0
+val int: 3
+val flt: 3.000000000
+last: 1.000000000
++: 4.000000000
+-: -2.000000000
+*: 3.000000000
+/: 0.333333333
+)",
+    R"(val str: 0.5
+val int: 0
+val flt: 0.500000000
+last: 3.000000000
++: 3.500000000
+-: 2.500000000
+*: 1.500000000
+/: 6.000000000
+)",
+    R"(val str: 100.0
+val int: 100
+val flt: 100.000000000
+last: 0.500000000
++: 100.500000000
+-: -99.500000000
+*: 50.000000000
+/: 0.005000000
+)",
+    R"(val str: 0.3
+val int: 0
+val flt: 0.300000000
+last: 100.000000000
++: 100.300000000
+-: 99.700000000
+*: 30.000000000
+/: 333.333333333
+)"};
+
+static const char* input[] = {"1.0", "3.0", "0.5", "100.0", "0.3"};
+
+TEST_F(FloatTests, TestDouble) {
   int i;
   double last = 0.0;
   double x;
-  printf("double\n");
-
-  for (i = 1; i < argc; ++i) {
-    printf("val str: %s\n", argv[i]);
-
-    x = strtod(argv[i], 0);
+  for (i = 0; i < 5; ++i) {
+    testing::internal::CaptureStdout();
+    printf("val str: %s\n", input[i]);
+    x = strtod(input[i], 0);
     PRINT_INT("val int", x);
     PRINT_FLOAT("val flt",9, x);
     PRINT_FLOAT("last", 9, last);
@@ -41,24 +152,20 @@ int main_double(int argc, char* argv[]) {
     PRINT_FLOAT("-", 9, last - x);
     PRINT_FLOAT("*", 9, last * x);
     PRINT_FLOAT("/", 9, last / x);
-    printf("\n");
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(double_output[i], output);
     last = x;
   }
-  return 0;
 }
 
-
-/* TODO(robertm): maybe use macro */
-int main_float(int argc, char* argv[]) {
+TEST_F(FloatTests, TestFloat) {
   int i;
   float last = 0.0;
   float x;
-  printf("float\n");
-
-  for (i = 1; i < argc; ++i) {
-    printf("val str: %s\n", argv[i]);
-
-    x = strtof(argv[i], 0);
+  for (i = 0; i < 5; ++i) {
+    testing::internal::CaptureStdout();
+    printf("val str: %s\n", input[i]);
+    x = strtof(input[i], 0);
     PRINT_INT("val int", x);
     PRINT_FLOAT("val flt",9, x);
     PRINT_FLOAT("last", 3, last);
@@ -66,15 +173,8 @@ int main_float(int argc, char* argv[]) {
     PRINT_FLOAT("-", 3, last - x);
     PRINT_FLOAT("*", 3, last * x);
     PRINT_FLOAT("/", 3, last / x);
-    printf("\n");
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(float_output[i], output);
     last = x;
   }
-  return 0;
-}
-
-
-int main(int argc, char* argv[]) {
-  main_float(argc, argv);
-  main_double(argc, argv);
-  return 0;
 }
